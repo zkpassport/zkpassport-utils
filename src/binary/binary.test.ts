33cbfe51e3bf83a0583f4f2f0f90ae735e83c66c
Michael Elliot <michael@digital.com.au>
2025-01-16 15:47:03 +0800

    add circleci

--- src/binary/binary.test.ts
+++ src/binary/binary.test.ts
@@ -1,4 +1,3 @@
-import { describe, it, expect } from "bun:test"
 import { Binary, HexString } from "./binary"
 
 describe("Binary", () => {
--- src/passport-reader/sod.test.ts
+++ src/passport-reader/sod.test.ts
@@ -1,5 +1,4 @@
 import { Binary } from "../binary"
-import { beforeAll, describe, expect, it } from "bun:test"
 import { SOD } from "./sod"
 
 const FIXTURE_EF_SOD =
