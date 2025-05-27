c2a3a26c4c311d28c9ae49c4b48098b810c40e96
Michael Elliot <michael@digital.com.au>
2025-05-27 11:47:50 +0800

    update fix-aliases.mjs to apply to both esm and cjs

--- scripts/fix-aliases.mjs
+++ scripts/fix-aliases.mjs
@@ -1,7 +1,9 @@
 import { replaceTscAliasPaths } from "tsc-alias"
 
-await replaceTscAliasPaths({
-  configFile: "tsconfig.json",
-  resolveFullPaths: true,
-  resolveFullExtension: ".js",
-})
+for (const configFile of ["tsconfig.json", "tsconfig.cjs.json"]) {
+  await replaceTscAliasPaths({
+    configFile,
+    resolveFullPaths: true,
+    resolveFullExtension: ".js",
+  })
+}
